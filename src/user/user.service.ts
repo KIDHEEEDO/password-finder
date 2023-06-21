c350ef7d581926bfa206b24e777cc9d5eb735a07
DOHEE <dohee212@idstrust.com>
2023-06-21 14:34:29 +0900

    [FEAT] 사용자 존재 여부 기능 개별 함수로 분리

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,9 +4,7 @@ import { UserModel } from 'SRC/user/entity/user.model';
 import { IntegrateException } from 'EXCEPTION/integrateException';
 import { ErrCode } from 'EXCEPTION/errCode';
 import { ErrMsg } from 'EXCEPTION/errMsg';
-import { sign } from 'jsonwebtoken';
 import { EnvService } from 'SRC/env/env.service';
-import { Env } from 'SRC/env/dataTypes/types/env.type';
 import { HashUtil } from 'COMMON/utils/hash.util';
 import { JwtUtil } from 'COMMON/utils/jwt.util';
 
@@ -24,14 +22,19 @@ export class UserService {
     };
 
     public login = async (email: string, password: string) => {
-        const emailUser = await this.userRepository.findOneByFilter({ email });
-        if (!emailUser) throw new Error('꺼져');
+        const emailUser = await this.validateUser({ email });
 
         const isMatched = await HashUtil.comparePassword(password, emailUser.password);
-        if (!isMatched) throw new Error('꺼져');
+        if (!isMatched) throw new IntegrateException(ErrCode.USER_NOT_FOUND, ErrMsg.USER_NOT_FOUND, HttpStatus.NOT_FOUND);
 
         const userId = emailUser.userId;
         const token = JwtUtil.issueToken({ userId });
         return { token };
+    };
+
+    private validateUser = async (filter: Partial<UserModel>) => {
+        const user = await this.userRepository.findOneByFilter(filter);
+        if (!user) throw new IntegrateException(ErrCode.USER_NOT_FOUND, ErrMsg.USER_NOT_FOUND, HttpStatus.NOT_FOUND);
+        return user;
     };
 }
